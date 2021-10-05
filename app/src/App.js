9ecdabd913374419f63526678c2d25d8976fac84
Ofer Bartal <blueofer@gmail.com>
2021-10-05 16:33:29 +0300

    Make App a class component (not function component)

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,15 @@
+import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  render() {
+    return <div className="App">
       <button>New game</button>
       <Board show></Board>
-    </div>
-  );
+    </div>;
+  }
 }
 
 export default App;
