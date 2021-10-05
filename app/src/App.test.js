4b92db84737718a255dc7ab4a0f6edd4cc7a5eea
Ofer Bartal <blueofer@gmail.com>
2021-10-05 16:31:21 +0300

    Remove React rotating logo

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders New game button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/New game/i);
   expect(linkElement).toBeInTheDocument();
 });
